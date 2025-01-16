f537c4b167577ae40a93477b18ac096b2c04b253
Joseph Ortiz <Se_sema_95@hotmail.com>
2025-01-16 13:45:49 +0000

    Create Testimonials Model

---
+++ about/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Testimonial
+
+class TestimonialForm(forms.ModelForm):
+  """
+  Form class for Users to submit Testimonials
+  """
+  class Meta:
+      """
+      Indicates the model used and field order
+      """
+      model = Testimonial
+      fields = ('body',)
--- about/models.py
+++ about/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Unapproved"), (1, "Approved"))
 
 class About(models.Model):
   """
@@ -9,3 +12,19 @@ class About(models.Model):
 
   def __str__(self):
     return self.title
+
+
+class Testimonial(models.Model):
+  """
+  Stores Testminoials submitted by users
+  """
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="testimonial_author")
+  body = models.TextField()
+  approved = models.IntegerField(choices=STATUS, default=0)
+  created_on = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    ordering = ["created_on"]
+
+  def __str__(self):
+    return f"Testimonial submitted by {self.author}"
