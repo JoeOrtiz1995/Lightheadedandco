71797de17add73623222076066ae88f32f6ed184
Joseph Ortiz <Se_sema_95@hotmail.com>
2025-01-19 12:52:58 +0000

    Add code to remove items from bag, update subtotal row to reflect correct price

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 def view_bag(request):
@@ -22,5 +22,32 @@ def add_to_bag(request, item_id):
     bag[item_id] = quantity
 
   request.session['bag'] = bag
-  print(request.session['bag'])
+  # print(request.session['bag'])
   return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+  """
+  Updates the quantity of a specified product in the bag
+  """
+  quantity = int(request.POST.get('quantity'))
+  bag = request.session.get('bag', {})
+
+  if quantity > 0:
+    bag[item_id] = quantity
+  else:
+    bag.pop(item_id)
+
+  request.session['bag'] = bag
+  return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+  """
+  Removes a specified product in the bag
+  """
+  bag = request.session.get('bag', {})
+  bag.pop(item_id)
+
+  request.session['bag'] = bag
+  return HttpResponse(status=200)
