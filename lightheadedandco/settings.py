81b827cfe191f14a6090f714f8bf431331917be8
Joseph Ortiz <Se_sema_95@hotmail.com>
2025-01-20 13:10:36 +0000

    Create checkout app & plan models

--- lightheadedandco/settings.py
+++ lightheadedandco/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s90lb__uyma(%z!a9v%2)nqew2x4qq*c575iuo0xg$+^y67zds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-joeortiz199-lightheaded-wn49fz64zxr.ws.codeinstitute-ide.net',]
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'about',
     'bag',
+    'checkout',
     'home',
     'products',
 
