7520e057e6f48d5c275dd4c86cc1860fa7ec0aeb
Joseph Ortiz <Se_sema_95@hotmail.com>
2025-01-26 14:47:37 +0000

    Finish adding webhooks, tested and works ok

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -78,6 +78,7 @@ form.addEventListener('submit', function (ev) {
           address: {
             line1: $.trim(form.street_address1.value),
             city: $.trim(form.town_or_city.value),
+            state: $.trim(form.county.value),
             country: $.trim(form.country.value),
           }
         }
@@ -89,6 +90,7 @@ form.addEventListener('submit', function (ev) {
           line1: $.trim(form.street_address1.value),
           city: $.trim(form.town_or_city.value),
           postal_code: $.trim(form.postcode.value),
+          state: $.trim(form.county.value),
           country: $.trim(form.country.value),
         }
       },
