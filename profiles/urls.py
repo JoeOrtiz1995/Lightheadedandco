717dc8f5f4e14412743d34459519f22e439add13
Joseph Ortiz <Se_sema_95@hotmail.com>
2025-01-29 19:01:20 +0000

    Create view and template for admin to add products

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
     path('wishlist', views.view_wishlist, name='view_wishlist'),
-    path('wishlist/add/<item_id>', views.add_to_wishlist, name='add_to_wishlist'),
-    path('wishlist/remove/<item_id>', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path('wishlist/add/<int:item_id>', views.add_to_wishlist, name='add_to_wishlist'),
+    path('wishlist/remove/<int:item_id>', views.remove_from_wishlist, name='remove_from_wishlist'),
 ]
