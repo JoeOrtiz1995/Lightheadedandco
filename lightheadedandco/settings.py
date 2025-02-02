74ba08d257a6425d2c4419dbd1e603c15645769c
Joseph Ortiz <Se_sema_95@hotmail.com>
2025-02-02 22:13:45 +0000

    Attempts deployment

--- lightheadedandco/settings.py
+++ lightheadedandco/settings.py
@@ -31,9 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = [
-    '8000-joeortiz199-lightheaded-v44kgywdt7o.ws.codeinstitute-ide.net',
-    '127.0.0.1', '.herokuapp.com',]
+ALLOWED_HOSTS = ['8000-joeortiz199-lightheaded-v44kgywdt7o.ws.codeinstitute-ide.net', '127.0.0.1', '.herokuapp.com',]
 
 # Application definition
 INSTALLED_APPS = [
