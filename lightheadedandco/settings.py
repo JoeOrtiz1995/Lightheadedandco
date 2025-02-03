817f35c4b69c9ed4523a622b449fcf3a9d299a1b
Joseph Ortiz <Se_sema_95@hotmail.com>
2025-02-03 15:37:42 +0000

    Further styles applied

--- lightheadedandco/settings.py
+++ lightheadedandco/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-joeortiz199-lightheaded-v44kgywdt7o.ws.codeinstitute-ide.net', '127.0.0.1', '.herokuapp.com',]
 
